ae638f34cd6c79a15b0fcd4391e102ab1b59f08d
ghassan <ghassanalassadi@outlook.com>
2022-07-05 18:31:48 +1000

    started project

---
+++ assets/js/style.js
@@ -0,0 +1,4 @@
+var apiKey = "7e4c79b601721f061335dfefeb6654a7";
+var latitude;
+var longitude;
+var weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
