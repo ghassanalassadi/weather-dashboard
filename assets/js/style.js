c214ce7e552f788c09f5c55d58a75744adb20a2b
ghassan <ghassanalassadi@outlook.com>
2022-07-07 18:35:47 +1000

    bootstrap modifications

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,4 +1,4 @@
-var apiKey = "7e4c79b601721f061335dfefeb6654a7";
-var latitude;
-var longitude;
-var weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+let apiKey = "7e4c79b601721f061335dfefeb6654a7";
+let latitude;
+let longitude;
+let weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
